3807cdcd27987941175b90b16db35f3624ebbfbf
Cole Oliva <olivac2@wwu.edu>
2023-12-31 23:44:52 -0800

    added comments for ease of use

--- calc.java
+++ calc.java
@@ -11,24 +11,28 @@ import java.awt.*;
 
 public class calc {
     public static void main(String[] args) {
+
+        // initializing popup window
         JFrame frame = new JFrame("Chat Frame");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(350, 150);
         frame.setLocationRelativeTo(null);
 
-        // first number panel
+        // first number panel - prompting and entering a number
         JPanel panel = new JPanel();
         JLabel label = new JLabel("Enter First Number");
         JTextField tf = new JTextField(10); // accepts upto 10 characters
         panel.add(label);
         panel.add(tf);
 
+        // second number panel
         JPanel panel2 = new JPanel();
         JLabel label2 = new JLabel("Enter Second Number");
         JTextField tf2 = new JTextField(10); // accepts upto 10 characters
         panel2.add(label2);
         panel2.add(tf2);
 
+        // the panel with the calculation buttons
         JPanel panel3 = new JPanel();
         JButton add = new JButton("add");
         JButton subtract = new JButton("subtract");
@@ -47,6 +51,7 @@ public class calc {
 
         JButton[] operation = new JButton[]{add, subtract, multiply, divide};
 
+        // adding an action to each button if pressed
         for (int i = 0; i < operation.length; i++) {
             JButton op = operation[i];
             op.addActionListener(new java.awt.event.ActionListener() {
@@ -56,6 +61,7 @@ public class calc {
                     String num2 = tf2.getText();
                     Integer first = Integer.parseInt(num);
                     Integer second = Integer.parseInt(num2);
+
                     double total = 0;
                     if (op.equals(add)) {
                         total = first + second;
@@ -66,6 +72,7 @@ public class calc {
                     } else if (op.equals(divide)) {
                         total = 1.0 * first / second;
                     }
+
                     JOptionPane.showMessageDialog(frame, "The total is: " + total);
                 }
             });
