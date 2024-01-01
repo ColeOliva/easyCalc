0bcc4a75d7fefe87dfae3e7510741cd828e687c7
Cole Oliva <olivac2@wwu.edu>
2023-12-31 18:48:08 -0800

    class skeleton

--- calc.java
+++ calc.java
@@ -1,1 +1,15 @@
-testing to see if this ms opens in vscode
+/*
+ * Author: Cole Oliva
+ * Date: 12/2023
+ * Purpose: the first of hopefully many projects, gaining experience with coding. 
+ */
+
+import java.util.*;
+import java.io.*;
+import javax.swing.*;
+
+public class calc {
+    public static void main(String[] args) {
+
+    }
+}
